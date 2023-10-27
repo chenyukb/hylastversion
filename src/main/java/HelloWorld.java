0cb45bfcc12d461b581a352dda6924874a2f647e
chenyukb <92746141+chenyukb@users.noreply.github.com>
2023-10-28 01:58:39 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("push 分支!???!!!devdev");
+    System.out.println("push 分支!???!!!devdevdev");
     System.out.println("Hello World!");
   }
 
